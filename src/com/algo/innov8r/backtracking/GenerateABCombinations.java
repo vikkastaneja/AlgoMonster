f8a6255580f9897211914f1f247b90235d9de362
vikkastaneja <vikkastaneja@users.noreply.github.com>
2024-06-19 00:03:47 -0700

    backtracking: Generate AB Combinations

---
+++ src/com/algo/innov8r/backtracking/GenerateABCombinations.java
@@ -0,0 +1,69 @@
+package com.algo.innov8r.backtracking;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Given a non-negative integer n, find all n-letter words composed by 'a' and 'b', return them in a list of strings in lexicographical order.
+ */
+public class GenerateABCombinations {
+    public static List<String> letterCombination(int n) {
+        List<String> returnString = new ArrayList<>();
+        dfs(n, returnString, 0, "");
+        return returnString;
+    }
+
+    private static void dfs(int n, List<String> list, int start, String tempString) {
+        if (start == n) {
+            list.add(tempString);
+            return;
+        }
+
+        for(String s : new String[] {"a", "b"}) {
+            dfs(n, list, start + 1, tempString + s);
+        }
+    }
+
+    public static void main(String[] args) {
+        List<String> res = letterCombination(2);
+
+        Collections.sort(res);
+        for (String line : res) {
+            System.out.println(line);
+        }
+
+        assertTrue(res.equals(Arrays.asList("aa", "ab", "ba", "bb")));
+
+        res = letterCombination(1);
+
+        Collections.sort(res);
+        for (String line : res) {
+            System.out.println(line);
+        }
+
+        assertTrue(res.equals(Arrays.asList("a", "b")));
+
+        res = letterCombination(3);
+
+        Collections.sort(res);
+        for (String line : res) {
+            System.out.println(line);
+        }
+
+        assertTrue(res.equals(Arrays.asList("aaa", "aab","aba","abb","baa","bab","bba","bbb")));
+
+        res = letterCombination(0);
+
+        Collections.sort(res);
+        for (String line : res) {
+            System.out.println(line);
+        }
+
+        assertTrue(res.equals(Arrays.asList("")));
+    }
+}
